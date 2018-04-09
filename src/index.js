d8b29be2d893104d01c6838a5ab0ac01efe7db4b
Yukiiro-Nite <e.kazuma@gmail.com>
2018-04-09 19:55:58 -0400

    Added api skeleton

---
+++ src/index.js
@@ -0,0 +1,61 @@
+/*
+This interface was created using documentation here: http://wiki.vg/Mojang_API
+ */
+const got = require('got');
+
+module.exports = {
+  getStatus,
+  getUUID,
+  getUUIDs,
+  getNames,
+  getProfile,
+  changeSkin,
+  uploadSkin,
+  resetSkin,
+  getStatistics
+};
+
+function getStatus() {
+  console.log('getStatus is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function getUUID() {
+  console.log('getUUID is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function getUUIDs() {
+  console.log('getUUIDs is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function getNames() {
+  console.log('getNames is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function getProfile() {
+  console.log('getProfile is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function changeSkin() {
+  console.log('changeSkin is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function uploadSkin() {
+  console.log('uploadSkin is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function resetSkin() {
+  console.log('resetSkin is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
+
+function getStatistics() {
+  console.log('getStatistics is not yet implemented!');
+  return Promise.reject('Not Implemented!');
+}
